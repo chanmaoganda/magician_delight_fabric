a3597bdde97d311f0f226d1df00f613d07872834
chanmaoganda <avaniawang@outlook.com>
2023-09-16 15:40:11 +0800

    found that the same Registry.register cannot register many at once

--- src/main/java/com/chanmaoganda/item/Initializer.java
+++ src/main/java/com/chanmaoganda/item/Initializer.java
@@ -1,24 +1,12 @@
 package com.chanmaoganda.item;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 public class Initializer implements ModInitializer {
     public static final String MOD_ID = "magician_delight";
 
-    public static List<ModItemBase> ModItems = new ArrayList<>();
-
-    // item instances
-    public static final ModItemBase STONE_OF_ORIGIN = new ModItemBase("stone_of_origin", new FabricItemSettings());
-
     @Override
     public void onInitialize() {
-        for(ModItemBase item : ModItems){
-            RegisterUtils.registerModItems(item);
-        }
+        RegisterUtils.RegisterHandler();
     }
 }
--- src/main/java/com/chanmaoganda/item/RegisterUtils.java
+++ src/main/java/com/chanmaoganda/item/RegisterUtils.java
@@ -1,16 +1,27 @@
 package com.chanmaoganda.item;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
 
 public class RegisterUtils {
-    public static Item registerModItems(ModItemBase item){
-        return itemRegister(item, item.ItemName);
+    public static final Item STONE_OF_ORIGIN = registerModItems(new Item(new FabricItemSettings()), "stone_of_origin");
+    public static final Item RAW_STONE_OF_ORIGIN = registerModItems(new Item(new FabricItemSettings()), "raw_stone_of_origin");
+
+    private static void AddItemToIngredientItemGroup(FabricItemGroupEntries entries){
+        entries.add(STONE_OF_ORIGIN);
+        entries.add(RAW_STONE_OF_ORIGIN);
+    }
+    private static Item registerModItems(Item item, String name){
+        return Registry.register(Registries.ITEM, new Identifier(Initializer.MOD_ID, name), item);
     }
 
-    private static Item itemRegister(Item item, String name){
-        return Registry.register(Registries.ITEM, new Identifier(Initializer.MOD_ID, name), item);
+    public static void RegisterHandler(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(RegisterUtils::AddItemToIngredientItemGroup);
     }
 }
